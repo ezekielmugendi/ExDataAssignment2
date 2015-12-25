011d988cb7693ef0f2753a60ab2146a0aae553c0
James Kelleher <jk3047@nyu.edu>
2015-12-25 10:23:03 -0700

    first plot

---
+++ plot1.R
@@ -0,0 +1,20 @@
+library(dplyr)
+
+if (!file.exists("Source_Classification_Code.rds") | !file.exists("summarySCC_PM25.rds")) {
+  dataUrl = "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_data.zip"
+  tmp = tempfile()
+  download.file(dataUrl, tmp)
+  unzip(tmp)
+  unlink(tmp)
+}
+
+if (!("nei" %in% ls())) {
+  nei = readRDS("summarySCC_PM25.rds")
+}
+
+emissionSummary = ddply(nei, .(year), summarise, totalEmissions = sum(Emissions))
+
+png("plot1.png", width = 450, height = 450)
+with(emissionSummary, plot(year, totalEmissions, xlab = "Year", ylab = "Total PM2.5 Emissions (tons)", type = "l", col = "red", lwd = 3, xaxt = "n"))
+axis(side = 1, at = emissionSummary$year)
+dev.off()
