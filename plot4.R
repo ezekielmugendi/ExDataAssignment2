bda7abdb7298c8847f5df6a6cb2784d6ac7ff62d
James Kelleher <jk3047@nyu.edu>
2015-12-25 14:10:36 -0700

    added call to ggplot2 library

--- plot4.R
+++ plot4.R
@@ -1,4 +1,5 @@
 library(dplyr)
+library(ggplot2)
 
 if (!file.exists("Source_Classification_Code.rds") | !file.exists("summarySCC_PM25.rds")) {
   dataUrl = "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_data.zip"
