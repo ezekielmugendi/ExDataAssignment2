193b27f1f63272f99a73235c7caa7262f47827f9
James Kelleher <jk3047@nyu.edu>
2015-12-25 14:06:47 -0700

    finished plot5

---
+++ plot5.R
@@ -0,0 +1,21 @@
+library(dplyr)
+
+if (!file.exists("Source_Classification_Code.rds") | !file.exists("summarySCC_PM25.rds")) {
+  dataUrl = "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_data.zip"
+  tmp = tempfile()
+  download.file(dataUrl, tmp)
+  unzip(tmp)
+  unlink(tmp)
+}
+
+nei = readRDS("summarySCC_PM25.rds")
+scc = readRDS("Source_Classification_Code.rds")
+
+scc_motor_vehicle_ids = scc$SCC[grep("^mobile - on-road .* vehicles$", scc$EI.Sector, ignore.case = T)]
+mvEmissions = ddply(subset(nei, fips == "24510" & SCC %in% scc_motor_vehicle_ids), .(year), summarize, totalEmissions = sum(Emissions))
+
+png("plot5.png")
+p = qplot(year, totalEmissions, data = mvEmissions, geom = "line", main = "Total Motor Vehicle-Related PM2.5 Emissions in Baltimore by Year", xlab = "Year", ylab = "Total PM2.5 Emissions (tons)")
+p = p + scale_x_continuous(breaks = mvEmissions$year)
+print(p)
+dev.off()
